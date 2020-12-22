9363cb6d28cc5e437466a6654217c6c522e1167b
jhosep98 <jhosep149@hotmail.com>
2020-12-22 13:23:35 -0300

    first steps
    Select elements of the HTML to display the results of the form submission.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+const result = document.querySelector("#resultado");
+const form = document.querySelector("#formulario");
+
+window.addEventListener("load", () => {
+  form.addEventListener("submit", searchWeather);
+});
+
+const searchWeather = (e) => {
+  e.preventDefault();
+
+  // validate
+
+  // consult API
+};
