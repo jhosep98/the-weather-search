44d7e95d21a82ae61c84053c494534c407a9e29e
jhosep98 <jhosep149@hotmail.com>
2020-12-22 15:23:43 -0300

    form validation
    validating the form with an error message when submitting the empty fields.

--- js/app.js
+++ js/app.js
@@ -9,7 +9,42 @@ window.addEventListener("load", () => {
 const searchWeather = (e) => {
   e.preventDefault();
 
-  // validate
+  // validate form
+  const city = document.querySelector("#ciudad").value;
+  const country = document.querySelector("#pais").value;
 
+  if (city === "" || country === "") {
+    showError("Both fields are required");
+    return;
+  }
   // consult API
 };
+
+function showError(message) {
+  const alertM = document.querySelector(".bg-red-100");
+  if (!alertM) {
+    const warningMessage = document.createElement("div");
+    warningMessage.classList.add(
+      "bg-red-100",
+      "border-red-400",
+      "text-red-700",
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-md",
+      "mx-auto",
+      "mt-6",
+      "text-center"
+    );
+    warningMessage.innerHTML = `
+      <strong class="font-bold">Error!</strong>
+      <span class="block">${message}</span>
+    `;
+    container.appendChild(warningMessage);
+
+    // Remove alert after 5 seconds
+    setTimeout(() => {
+      warningMessage.remove();
+    }, 3000);
+  }
+}
